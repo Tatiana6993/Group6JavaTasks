bbf30eac8bc68a6545e74caae9d8bf805fb4a03f
Kasana_Cydeo <integra1918@gmail.com>
2025-10-23 15:43:45 -0500

    Update task 1 is done

--- src/ROksana/oddOrEven.java
+++ src/ROksana/oddOrEven.java
@@ -3,5 +3,6 @@ package ROksana;
 public class oddOrEven {
     public static void main(String[] args) {
         System.out.println("This is ROksana");
+        System.out.println("Thats ok");
     }
    }
