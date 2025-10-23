e0d363cad772a0e496ee7c8e611097cd22b8df91
zekisaglam <zzekisaglam@gmail.com>
2025-10-23 17:10:37 -0400

    task is done

---
+++ src/Zeki/oddOrEven.java
@@ -0,0 +1,10 @@
+package Zeki;
+
+public class oddOrEven {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is Zeki");
+        System.out.println("Great Job!!");
+    }
+}
