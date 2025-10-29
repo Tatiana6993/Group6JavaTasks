ab182c5f8e6379e33f4961fd4e4cc07a85a001d3
TseringDickey <c_ring@live.com>
2025-10-29 12:39:25 -0400

    OddOrEven solved.

--- src/Tsering/oddOrEven.java
+++ src/Tsering/oddOrEven.java
@@ -2,9 +2,36 @@ package Tsering;
 
 public class oddOrEven {
 
+    /*
+    Write a method which can identify if a given number odd or even.
+    Ex:
+       Identify(5) --> "Odd"
+       Identify(6) --> "Even"
+     */
+
     public static void main(String[] args) {
 
-        System.out.println("This is Tsering Dickey.");
-        System.out.println("Good job!");
+            identifyNum(5);
+            identifyNum(6);
+
+        }
+
+        public static void identifyNum(int num) {
+            if (num % 2 == 0) {
+                System.out.println("Even");
+            } else {
+                System.out.println("Odd");
+            }
+        }
+
+        /*
+        public static void identifyNum(int num1) {
+            String result = (num % 2 == 0) ? "Even number" : "Odd number";
+            System.out.println(result);
+        }
+         */
     }
-}
+
+
+
+
