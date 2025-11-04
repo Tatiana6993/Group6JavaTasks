83b8db3aa9e97918982cf732449a4ca32bb1baca
Tatiana <tanya.travelingg@gmail.com>
2025-11-03 23:02:15 -0600

    Task2Finra completed.

--- src/weeklyTasksForGr6/Task2Finra.java
+++ src/weeklyTasksForGr6/Task2Finra.java
@@ -2,19 +2,40 @@ package weeklyTasksForGr6;
 
 public class Task2Finra {
 
+    public static void main(String[] args) {
 
+
+        printFINRA(1,30);
+    }
+
+
+    public static void printFINRA(int start,int end) {
+        for (int i = start; i <= end; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FINRA");
+
+            } else if (i % 3 == 0) {
+                System.out.println("FIN");
+
+            } else if (i % 5 == 0) {
+                System.out.println("RA");
+
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
 }
+
+
+
+
 /*
 Task 2, FINRA
-
-Write a Java method that follows these rules:
-
-Print all numbers from 1 to 30.
-
+Write a Java method that follows these rules.Print all numbers from 1 to 30.
 For numbers that are multiples of 3, print "FIN" instead of the number.
-
 For numbers that are multiples of 5, print "RA" instead of the number.
-
 For numbers that are multiples of both 3 and 5, print "FINRA" instead of the number.
+*/
 
- */
