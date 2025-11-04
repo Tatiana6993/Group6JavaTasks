8b00abb947179252bbc9f9c82afc8998d2cc72c6
SVO89 <dime4000@gmail.com>
2025-11-04 14:42:21 -0700

    Added FINRA task to my week2 branch with instructions.

---
+++ src/Silvio/week2/Finra.java
@@ -0,0 +1,11 @@
+package Silvio.week2;
+
+public class Finra {
+    /* Task 2, FINRA.
+    Write a Java method that follows these rules:
+    Print all numbers from 1 to 30.
+    For numbers that are multiples of 3, print "FIN" instead of the number.
+    For numbers that are multiples of 5, print "RA" instead of the number.
+    For numbers that are multiples of both 3 and 5, print "FINRA" instead of the number. */
+
+}
