a3ee109a95fe679fd548284796b1929039756fa1
SVO89 <dime4000@gmail.com>
2025-11-05 13:56:20 -0700

    Completed Finra task.

--- src/Silvio/week2/Finra.java
+++ src/Silvio/week2/Finra.java
@@ -8,4 +8,23 @@ public class Finra {
     For numbers that are multiples of 5, print "RA" instead of the number.
     For numbers that are multiples of both 3 and 5, print "FINRA" instead of the number. */
 
+    public static void main (String[] args){
+
+        finra(1,30);
+
+    }
+
+    static void finra (int start, int end){
+
+        for (int i = start; i <= end; i++){
+
+            if (i % 3 == 0 && i % 5 == 0){
+                System.out.println(i + " = FINRA");
+            } else if (i % 3 == 0){
+                System.out.println(i + " = FIN");
+            } else if (i % 5 == 0){
+                System.out.println(i + " = RA");
+            } else {System.out.println(i);}
+        }
+    }
 }
