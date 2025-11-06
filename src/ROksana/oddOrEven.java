a586d0b19e931f818714e49386ed659838343bd7
Kasana_Cydeo <integra1918@gmail.com>
2025-11-05 22:22:29 -0600

    Update task 1 updated

--- src/ROksana/oddOrEven.java
+++ src/ROksana/oddOrEven.java
@@ -1,8 +1,43 @@
 package ROksana;
 
+import java.util.Scanner;
+
 public class oddOrEven {
     public static void main(String[] args) {
-        System.out.println("This is ROksana");
-        System.out.println("Thats ok");
+        checkOddEven();
+
+        check1OddEven(47);
+        check1OddEven(10);
+
     }
-   }
+
+
+    public static void checkOddEven() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number please: ");
+
+        if (scanner.hasNextInt()) {
+            int number = scanner.nextInt();
+
+            if (number % 2 == 0) {
+                System.out.println(number + " is even.");
+            } else {
+                System.out.println(number + " is odd.");
+            }
+        } else {
+            System.err.println("Not valid input. Must be a number.");
+        }
+
+        scanner.close();
+    }
+        public static void check1OddEven(int number) {
+            if (number % 2 == 0) {
+                System.out.println(number + " is even.");
+            } else {
+                System.out.println(number + " is odd.");
+            }
+        }
+
+
+
+}
