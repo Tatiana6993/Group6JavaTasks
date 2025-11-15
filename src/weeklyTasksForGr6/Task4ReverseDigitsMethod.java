57abc206971098ab26b0857a33ccc05b3e4108e0
TATIANA PITIGOI <TANYA.TRAVELINGG@GMAIL.COM>
2025-11-15 15:55:00 -0600

    Tatiana (#41)
    * Task3SwappingNumbers is updated by using a method  .Pls review
    
    * Task3SwappingNumbers is updated by using a method  .Pls review

---
+++ src/weeklyTasksForGr6/Task4ReverseDigitsMethod.java
@@ -0,0 +1,9 @@
+package weeklyTasksForGr6;
+
+public class Task4ReverseDigitsMethod {
+
+    /*
+    Task 4,
+Write a return method that can reverse digits of a negative number and return it as int
+     */
+}
