00aed79b866a8fd0a0bd1ddfa8c71e5af3536ed0
Chakar77 <91497307+Chakar77@users.noreply.github.com>
2026-01-06 23:25:06 +0100

    all solutions for the Java coding tasks (#45)

---
+++ src/Mentor/Task_1_OddorEven.java
@@ -0,0 +1,26 @@
+package Mentor;
+/*
+      QUESTION 1 -Numbers  ---Odd & Even
+       Write a method which can identify if a given number odd or even
+
+       Ex:
+       identify(5) -->"Odd"
+       identify (6) --> "Even"
+      */
+public class Task_1_OddorEven {
+
+
+    public static String identify(int num) {
+        if (num % 2 == 0) {
+            return "Even";
+        }
+        return "Odd";
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(identify(5));
+
+    }
+}
